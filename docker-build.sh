839c83dd314ed2335558ebd2c8593b39ba0e9723
Toby Shi <toby3030@qq.com>
2023-06-29 23:12:13 +0800

    build one arch only

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-docker build -t "sysy:tobisc" .
+ARM_NAME="arm-linux-gnueabihf"
+RISCV_NAME="riscv64-linux-gnu"
+pkg="clang llvm lld gcc-$ARM_NAME gcc-$RISCV_NAME binutils-$ARM_NAME binutils-$RISCV_NAME"
+
+if [ -z "$1" ]; then
+    docker build -t "sysy:tobisc" .
+    exit 0
+elif [[ "$1" =~ "arm" ]]; then
+    pkg="$pkg qemu-user qemu-system-arm"
+elif [[ "$1" =~ "riscv" ]]; then
+    pkg="$pkg qemu-user qemu-system-riscv64"
+fi
+
+docker build --build-arg PACKAGES="$pkg" -t "sysy:tobisc" .
